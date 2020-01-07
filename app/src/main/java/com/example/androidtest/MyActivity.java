95d9e99c2a903e4936f694268d891bd9b377c83c
qing5201314 <57691250+qing5201314@users.noreply.github.com>
2020-01-07 17:30:01 +0800

    页面跳转代码

---
+++ app/src/main/java/com/example/androidtest/MyActivity.java
@@ -0,0 +1,22 @@
+package com.example.sx;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+/**
+ * 个人页面
+ */
+public class MyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+}
