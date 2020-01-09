1bc02e8047c46d711c33cbd6e358e90d799e6ef0
qing5201314 <57691250+qing5201314@users.noreply.github.com>
2020-01-09 11:13:10 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/androidtest/SrcActivity.java
@@ -0,0 +1,26 @@
+package com.example.lenovo.wenews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SrcActivity extends AppCompatActivity {
+    private Button btnback;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_src);
+
+        btnback = findViewById(R.id.btn_cancel);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SrcActivity.this,MainActivity_.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
